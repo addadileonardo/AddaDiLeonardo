af568ebf3dee108c2024e55078e64a197af89ec2
Andrea Troccolo <troccoloandrea01@gmail.com>
2020-03-27 16:57:31 +0100

    Homepage + tappe: creazione

---
+++ AddaDiLeonardo/AddaDiLeonardo/Views/Tappe/Tappa_02.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AddaDiLeonardo.Views.Tappe
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Tappa_02 : ContentPage
+    {
+        public Tappa_02()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AddaDiLeonardo/AddaDiLeonardo/Views/Tappe/Tappa_04.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AddaDiLeonardo.Views.Tappe
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Tappa_04 : ContentPage
+    {
+        public Tappa_04()
+        {
+            InitializeComponent();
+        }
+    }
+}
