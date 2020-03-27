865165667d5b477989278750bc3c160caa68e3f0
Andrea Troccolo <troccoloandrea01@gmail.com>
2020-03-27 16:31:30 +0100

    LabelRender

---
+++ AddaDiLeonardo/AddaDiLeonardo.Android/CustomLabelRender.cs
@@ -0,0 +1,26 @@
+﻿using AddaDiLeonardo.CustomControls;
+using AddaDiLeonardo.Droid;
+using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomLabel), typeof(CustomLabelRender))]
+namespace AddaDiLeonardo.Droid
+{
+    public class CustomLabelRender : LabelRenderer
+    {
+        public CustomLabelRender(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                //Control.JustificationMode = Android.Text.JustificationMode.InterWord;
+            }
+        }
+    }
+}
---
+++ AddaDiLeonardo/AddaDiLeonardo.iOS/CustomLabelTitleRender.cs
@@ -0,0 +1,20 @@
+﻿using AddaDiLeonardo.CustomControls;
+using AddaDiLeonardo.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomLabelTitle), typeof(CustomLabelTitleRender))]
+namespace AddaDiLeonardo.iOS
+{
+    public class CustomLabelTitleRender : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                //Control.TextAlignment = UITextAlignment.Justified;
+            }
+        }
+    }
+}
