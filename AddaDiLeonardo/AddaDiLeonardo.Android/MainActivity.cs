ed49a02829e4abda346907e0b694ff3f09faf30e
Riccardo Raffini <riccardo.raffini@gmail.com>
2020-03-31 23:18:54 +0200

    Database Commit contenente le modifiche relative alla gestione dei contenuti dell'app. +Contenuti.cs +Sezioni.cs +Tappa.cs +AsyncTaskException.cs +Constant.cs +Database.cs *App.xaml.cs
    Aggiunta file database in Android.Assets e iOS.Resources
    - Francese.db
    - Inglese.db
    - Italiano.db

--- AddaDiLeonardo/AddaDiLeonardo.Android/MainActivity.cs
+++ AddaDiLeonardo/AddaDiLeonardo.Android/MainActivity.cs
@@ -7,6 +7,10 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using LibVLCSharp.Forms.Shared;
+using Android.Content.Res;
+using Android.Graphics;
+using Java.IO;
+using System.IO;
 
 namespace AddaDiLeonardo.Droid
 {
@@ -23,12 +27,32 @@ namespace AddaDiLeonardo.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            //Database
+            ExtractDBFromAssets(dbname: "Italiano.db");
+            ExtractDBFromAssets(dbname: "Inglese.db");
+            ExtractDBFromAssets(dbname: "Francese.db");
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void ExtractDBFromAssets(string dbname)
+        {
+            string destination = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string filename = System.IO.Path.Combine(destination, dbname);
+
+            if (!System.IO.File.Exists(filename))
+            {
+                using (FileStream writeStream = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    Assets.Open(dbname).CopyTo(writeStream);
+                }
+            }
         }
     }
 }
