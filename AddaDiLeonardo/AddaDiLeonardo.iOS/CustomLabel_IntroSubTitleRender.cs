2098fc814fc73926aaec9350aed1653c1e40d714
Andrea Troccolo <troccoloandrea01@gmail.com>
2020-04-01 14:30:29 +0200

    Modifiche videoPlayer e label C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo.Android C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo.Android\AddaDiLeonardo.Android.csproj Edit C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo.Android\CustomLabel_IntroSubTitleRender.cs Add C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo.Android\CustomLabel_IntroTitleRender.cs Add C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo.iOS C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo.iOS\AddaDiLeonardo.iOS.csproj Edit C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo.iOS\CustomLabel_IntroSubTitleRender.cs Add C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo.iOS\CustomLabel_IntroTitleRender.cs Add C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo.csproj Edit C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo\App.xaml.cs Edit C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo\CustomControls C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo\CustomControls\CustomLabel_IntroSubTitle.cs Add C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo\CustomControls\CustomLabel_IntroTitle.cs Add C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo\MainPage.xaml Edit C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo\MainPage.xaml.cs Edit C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo\Views C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo\Views\PlayerPage.xaml Add C:\Users\andrea\Source\Repos\AddaDiLeonardo\AddaDiLeonardo\AddaDiLeonardo\Views\PlayerPage.xaml.cs Add

---
+++ AddaDiLeonardo/AddaDiLeonardo.iOS/CustomLabel_IntroSubTitleRender.cs
@@ -0,0 +1,20 @@
+﻿using AddaDiLeonardo.CustomControls;
+using AddaDiLeonardo.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomLabel_IntroSubTitle), typeof(CustomLabel_IntroSubTitleRender))]
+namespace AddaDiLeonardo.iOS
+{
+    public class CustomLabel_IntroSubTitleRender : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                //Control.TextAlignment = UITextAlignment.Justified;
+            }
+        }
+    }
+}
---
+++ AddaDiLeonardo/AddaDiLeonardo/CustomControls/CustomLabel_IntroTitle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AddaDiLeonardo.CustomControls
+{
+    public class CustomLabel_IntroTitle : Label
+    {
+        public CustomLabel_IntroTitle()
+        {
+            FontFamily = Device.RuntimePlatform == Device.iOS ? "RobotoCondensed-Bold" : Device.RuntimePlatform == Device.Android ? "RobotoCondensed-Bold.ttf#RobotoCondensed-Bold" : "Assets/RobotoCondensed-Bold.ttf#RobotoCondensed-Bold";
+            FontSize = 28;
+            TextColor = Color.Black;
+        }
+    }
+}
